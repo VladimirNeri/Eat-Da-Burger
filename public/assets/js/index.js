4169a348b38dc48ccaa5c9a64171011f52a33876
obscura101 <vlad.neri@gmail.com>
2019-06-27 14:52:46 -0700

    added base code

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -31,7 +31,7 @@ $(function() {
       };
   
       // Send the POST request.
-      $.ajax("/api/cats", {
+      $.ajax("/api/burgers/" + id {
         type: "POST",
         data: newCat
       }).then(
