32c61146504d76806f0c9c6b372d690cc461c6e4
obscura101 <vlad.neri@gmail.com>
2019-07-02 12:13:39 -0700

    removed parser

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var bodyParser = require("body-parser");
+//var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var path = require("path");
 var timeout = require("connect-timeout");
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // not sure about this one... 
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.urlencoded({ extended: true }));
 
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
