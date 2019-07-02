9bbe67b24caa53f9d532f5bfd0b0ab1aa89ba783
obscura101 <vlad.neri@gmail.com>
2019-07-02 09:48:09 -0700

    updated

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var path = require("path");
+var timeout = require("connect-timeout");
 
 var PORT = process.env.PORT || 8080;
 var app = express();
